22fdb082cb4288d2a34553ddebcdc6cd517e9b72
R.Omkarnath <91599476+Omkarnathscat@users.noreply.github.com>
2021-11-03 22:02:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+function multi(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      num1= parseFloat(num1);
+      num2= parseFloat(num2);
+      var answer = num1 * num2;
+      document.write(answer)
+};
+function div(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      num1= parseFloat(num1);
+      num2= parseFloat(num2);
+      var answer = num1 / num2;
+      document.write(answer)
+};
+function add(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      num1= parseFloat(num1);
+      num2= parseFloat(num2);
+      var answer = num1 + num2;
+      document.write(answer)
+};
+function sub(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      num1= parseFloat(num1);
+      num2= parseFloat(num2);
+      var answer = num1 - num2;
+      document.write(answer)
+};
+function rm(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      num1= parseFloat(num1);
+      num2= parseFloat(num2);
+      var answer = num1 % num2;
+      document.write(answer)
+};
